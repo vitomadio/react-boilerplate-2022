8e9239578d98a35d78c28ee5a8324c7014b5f3e8
vitomadio <vitomadio@hotmail.com>
2022-01-02 20:25:48 +0100

    Add and configure eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const HelloWorld: React.FC = (): JSX.Element => {
-  return <h1>Hello World</h1>;
+	return <h1>Hello World</h1>;
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
